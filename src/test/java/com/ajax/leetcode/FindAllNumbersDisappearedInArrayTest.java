fe149af11dfa04e5646771628fa3d61916a5de3d
Michel Atucha <miche.atucha@gmail.com>
2021-04-04 20:59:36 -0500

    Find All Numbers Disappeared in an Array

---
+++ src/test/java/com/ajax/leetcode/FindAllNumbersDisappearedInArrayTest.java
@@ -0,0 +1,22 @@
+package com.ajax.leetcode;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FindAllNumbersDisappearedInArrayTest {
+    @Test
+    public void test() {
+        FindAllNumbersDisappearedInArray executor = new FindAllNumbersDisappearedInArray();
+
+        int[] input = new int[] {4,3,2,7,8,2,3,1};
+        List<Integer> expected = List.of(5, 6);
+        List<Integer> result = executor.findDisappearedNumbers(input);
+        Assertions.assertEquals(expected, result);
+
+        input = new int[] {1,1};
+        expected = List.of(2);
+        result = executor.findDisappearedNumbers(input);
+        Assertions.assertEquals(expected, result);
+    }
+}
