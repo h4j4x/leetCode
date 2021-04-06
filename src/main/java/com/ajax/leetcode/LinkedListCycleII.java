9429532cae9ed0bf99327e055440e8ffc645f6d7
Michel Atucha <miche.atucha@gmail.com>
2021-04-06 18:55:43 -0500

    linked list cycle II

---
+++ src/main/java/com/ajax/leetcode/LinkedListCycleII.java
@@ -0,0 +1,32 @@
+package com.ajax.leetcode;
+
+import com.ajax.leetcode.auxiliary.ListNode;
+
+// https://leetcode.com/problems/linked-list-cycle-ii/
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head != null) {
+            int lastPos = -1;
+            ListNode node = head;
+            while (node != null) {
+                int pos = positionOf(node, head);
+                if (pos <= lastPos) {
+                    return node;
+                }
+                lastPos = pos;
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
+    private int positionOf(ListNode node, ListNode head) {
+        int pos = 0;
+        ListNode value = head;
+        while (value != node) {
+            pos++;
+            value = value.next;
+        }
+        return pos;
+    }
+}
