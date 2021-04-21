c6b1784aa7a550a0a674c2f75d3ad3de8144dff6
Michel Atucha <miche.atucha@gmail.com>
2021-04-21 17:38:00 -0500

    Remove Nth Node From End of List

---
+++ src/test/java/com/ajax/leetcode/RemoveNthNodeFromEndListTest.java
@@ -0,0 +1,50 @@
+package com.ajax.leetcode;
+
+import com.ajax.leetcode.auxiliary.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RemoveNthNodeFromEndListTest {
+    @Test
+    public void test() {
+        RemoveNthNodeFromEndList executor = new RemoveNthNodeFromEndList();
+
+        ListNode head = createList(1, 2, 3, 4, 5);
+        int n = 2;
+        ListNode expected = createList(1, 2, 3, 5);
+        ListNode result = executor.removeNthFromEnd(head, n);
+        Assertions.assertEquals(expected, result);
+
+        head = createList(1);
+        n = 1;
+        result = executor.removeNthFromEnd(head, n);
+        Assertions.assertNull(result);
+
+        head = createList(1, 2);
+        n = 1;
+        expected = createList(1);
+        result = executor.removeNthFromEnd(head, n);
+        Assertions.assertEquals(expected, result);
+
+        head = createList(1, 2);
+        n = 2;
+        expected = createList(2);
+        result = executor.removeNthFromEnd(head, n);
+        Assertions.assertEquals(expected, result);
+    }
+
+    private ListNode createList(int... values) {
+        ListNode head = null;
+        ListNode node = null;
+        for (int value : values) {
+            if (head == null) {
+                head = node = new ListNode(value);
+            } else {
+                ListNode next = new ListNode(value);
+                node.next = next;
+                node = next;
+            }
+        }
+        return head;
+    }
+}
