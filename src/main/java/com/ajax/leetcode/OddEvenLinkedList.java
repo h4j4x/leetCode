2a7a10e7629118272bfc01eddfd5e9b06f1d9eef
Michel Atucha <miche.atucha@gmail.com>
2021-04-28 16:30:53 -0500

    Odd Even Linked List

---
+++ src/main/java/com/ajax/leetcode/OddEvenLinkedList.java
@@ -0,0 +1,41 @@
+package com.ajax.leetcode;
+
+import com.ajax.leetcode.auxiliary.ListNode;
+
+// https://leetcode.com/problems/odd-even-linked-list/
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode theHead = null;
+        ListNode headLast = null;
+        ListNode evenHead = null;
+        ListNode evenLast = null;
+        ListNode node = head;
+        boolean even = false;
+        while (node != null) {
+            ListNode newNode = new ListNode(node.val);
+            if (even) {
+                if (evenLast != null) {
+                    evenLast.next = newNode;
+                }
+                evenLast = newNode;
+                if (evenHead == null) {
+                    evenHead = evenLast;
+                }
+            } else {
+                if (headLast != null) {
+                    headLast.next = newNode;
+                }
+                headLast = newNode;
+                if (theHead == null) {
+                    theHead = headLast;
+                }
+            }
+            node = node.next;
+            even = !even;
+        }
+        if (headLast != null) {
+            headLast.next = evenHead;
+        }
+        return theHead;
+    }
+}
