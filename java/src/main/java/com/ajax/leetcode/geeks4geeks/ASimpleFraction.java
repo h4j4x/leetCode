f0def4c148740fe881e0fc5979b6eae0cfc26d29
Michel Atucha <miche.atucha@gmail.com>
2021-09-01 11:50:54 -0500

    a simple fraction

---
+++ java/src/main/java/com/ajax/leetcode/geeks4geeks/ASimpleFraction.java
@@ -0,0 +1,47 @@
+package com.ajax.leetcode.geeks4geeks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ASimpleFraction {
+    public static String fractionToDecimal(int numerator, int denominator) {
+        if (denominator == 0) {
+            return "";
+        }
+        int value = numerator / denominator;
+        int remainder = numerator % denominator;
+        String divider = "0";
+        if (remainder > 0) {
+            final Map<Integer, Integer> remainderIndexMap = new HashMap<>();
+            final List<Integer> values = new ArrayList<>();
+            while (remainder != 0
+                && !remainderIndexMap.containsKey(remainder)) {
+                remainderIndexMap.put(remainder, values.size());
+                remainder *= 10;
+                int decValue = remainder / denominator;
+                remainder = remainder % denominator;
+                values.add(decValue);
+            }
+            int index = remainderIndexMap.getOrDefault(remainder, -1);
+            StringBuilder result = new StringBuilder();
+            if (index >= 0) {
+                for (int i = 0; i < index; i++) {
+                    result.append(values.get(i));
+                }
+                result.append("(");
+                for (int i = index; i < values.size(); i++) {
+                    result.append(values.get(i));
+                }
+                result.append(")");
+            } else {
+                for (Integer decValue : values) {
+                    result.append(decValue);
+                }
+            }
+            divider = result.toString();
+        }
+        return value + "." + divider;
+    }
+}
---
+++ java/src/test/java/com/ajax/leetcode/geeks4geeks/ASimpleFractionTest.java
@@ -0,0 +1,24 @@
+package com.ajax.leetcode.geeks4geeks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ASimpleFractionTest {
+    @Test
+    public void test() {
+        String value = ASimpleFraction.fractionToDecimal(1, 3);
+        Assertions.assertEquals("0.(3)", value);
+
+        value = ASimpleFraction.fractionToDecimal(5, 2);
+        Assertions.assertEquals("2.5", value);
+
+        value = ASimpleFraction.fractionToDecimal(10, 2);
+        Assertions.assertEquals("5.0", value);
+
+        value = ASimpleFraction.fractionToDecimal(4, 9);
+        Assertions.assertEquals("0.(4)", value);
+
+        value = ASimpleFraction.fractionToDecimal(16, 13);
+        Assertions.assertEquals("1.(230769)", value);
+    }
+}
