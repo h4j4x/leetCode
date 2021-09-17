d6d9ad2859c8c448aeecbf838c26acf192d228d0
Ajax <miche.atucha@gmail.com>
2021-09-17 18:13:55 -0500

    majority element

---
+++ java/src/main/java/com/ajax/leetcode/geeks4geeks/MajorityElement.java
@@ -0,0 +1,32 @@
+package com.ajax.leetcode.geeks4geeks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MajorityElement {
+    public static int majorityElement(int[] a, int size) {
+        if (size == 0) {
+            return -1;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            Integer value = map.getOrDefault(a[i], 0);
+            map.put(a[i], value + 1);
+        }
+        int max = -1;
+        int maxCount = 0;
+        Set<Integer> keys = map.keySet();
+        for (Integer key : keys) {
+            Integer value = map.get(key);
+            if (value > maxCount) {
+                max = key;
+                maxCount = value;
+            }
+        }
+        if (maxCount > size / 2) {
+            return max;
+        }
+        return -1;
+    }
+}
---
+++ java/src/test/java/com/ajax/leetcode/geeks4geeks/MajorityElementTest.java
@@ -0,0 +1,17 @@
+package com.ajax.leetcode.geeks4geeks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MajorityElementTest {
+    @Test
+    public void test() {
+        int[] array = {1, 2, 3};
+        int value = MajorityElement.majorityElement(array, array.length);
+        Assertions.assertEquals(-1, value);
+
+        array = new int[] {3, 1, 3, 3, 2};
+        value = MajorityElement.majorityElement(array, array.length);
+        Assertions.assertEquals(3, value);
+    }
+}
